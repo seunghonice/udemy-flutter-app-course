8197e125935d75007229741d696610afa20403e8
Seungho Hong <seunghonice@gmail.com>
2022-02-03 09:54:01 +0900

    chore: async 에 대해 공부하자
    - async/await 함께 사용한다. await 사용 시 future를 벗길 수 있다. 해당 함수 콜러가 해당 함수를 기다렸다가 다음 문장을 실행한다.
    - future.then 콜백을 사용하자. 안전하다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:udemy_flutter_app_course/widgets/SIForm.dart';
+import 'package:udemy_flutter_app_course/widgets/notekeeper/note_list.dart';
+import 'dart:async';
 
-void main() {
+void main() async {
   /**
    * FavoriteCity
    */
@@ -13,17 +14,67 @@ void main() {
   // );
 
   /**
-   * Calculator App
+   * SIForm
    */
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Calculator App",
-      home: SIForm(),
-    theme: ThemeData(
-      primaryColor: Colors.amber,
-      accentColor: Colors.amberAccent,
-      backgroundColor: Colors.black12,
-      buttonColor: Colors.amberAccent
-    ),
+  // runApp(MaterialApp(
+  //     debugShowCheckedModeBanner: false,
+  //     title: "Calculator App",
+  //     home: SIForm(),
+  //   theme: ThemeData(
+  //     primaryColor: Colors.amber,
+  //     accentColor: Colors.amberAccent,
+  //     backgroundColor: Colors.black12,
+  //     buttonColor: Colors.amberAccent
+  //   ),
+  // ));
+
+  /**
+   * NoteKeeper
+   */
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "NoteKeeper App",
+    home: NoteKeeper(),
   ));
+
+
+  program2();
+  program1();
+
+}
+
+program1() {
+  print("Main program1: Starts");
+
+  printFileContent1();
+
+  print("Main program1: Ends");
+}
+
+program2() async {
+  print("Main program2: Starts");
+
+  await printFileContent2();
+
+  print("Main program2: Ends");
+}
+
+printFileContent1() {
+  Future<String> fileContent = downloadAFile();
+  fileContent.then((fileContent) =>
+      print("The content of the file is --> $fileContent")
+  );
+}
+
+printFileContent2() async {
+  String fileContent = await downloadAFile();
+  print("The content of the file is --> $fileContent");
+}
+
+Future<String> downloadAFile() {
+  Future<String> result = Future.delayed(Duration(seconds: 6), () {
+    return "My secret file content";
+  });
+
+  return result;
 }
